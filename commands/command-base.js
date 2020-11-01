d058536faa640752cadc595251fce49ab8e4c2c6
Joshua Foxworthy <foxworthyjoshua12@gmail.com>
2020-11-01 05:19:20 -0500

    edit econ

--- commands/command-base.js
+++ commands/command-base.js
@@ -6,7 +6,7 @@
  */
 
 const commandPrefixSchema = require('@schemas/command-prefix-schema')
-const { prefix: globalPrefix } = process.env.PREFIX
+const { prefix: globalPrefix } = require('@root/config.json')
 const guildPrefixes = {} // { 'guildId' : 'prefix' }
 
 const validatePermissions = (permissions) => {
