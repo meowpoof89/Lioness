8ab7905a1e8fc9cb9670ff7b3543f02332db7524
Joshua Foxworthy <foxworthyjoshua12@gmail.com>
2020-11-01 21:28:44 -0500

    update

--- cmds/moderation/invite.js
+++ cmds/moderation/invite.js
@@ -1,15 +1,15 @@
-const Commando = require('discord.js-commando')
+// const Commando = require('discord.js-commando')
 
-module.exports = class KickCommand extends Commando.Command {
-  constructor(client) {
-    super(client, {
-      name: 'invite',
-      group: 'moderation',
-      memberName: 'invite',
-      description: 'sends a URL link for lioness',
-      clientPermissions: ['INVITE'],
-      userPermissions: ['INVTIE'],
-    })
-  }
+// module.exports = class KickCommand extends Commando.Command {
+//   constructor(client) {
+//     super(client, {
+//       name: 'invite',
+//       group: 'moderation',
+//       memberName: 'invite',
+//       description: 'sends a URL link for lioness',
+//       clientPermissions: ['INVITE'],
+//       userPermissions: ['INVTIE'],
+//     })
+//   }
   
-}
+// }
